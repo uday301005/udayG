c18f35f167bf196537e6d8d81c09faa7a7497e23
vueuday <sukuna9540641428@gmail.com>
2025-08-15 03:53:52 +0000

    nothing

--- 01_basics/maket.js
+++ 01_basics/maket.js
@@ -1,1 +1,4 @@
 console.log("Hii i am Master");
+console.log("I am a JavaScript file");
+console.log("I am learning how to make a JavaScript file");
+console.log("This is the first file in the basics folder");
